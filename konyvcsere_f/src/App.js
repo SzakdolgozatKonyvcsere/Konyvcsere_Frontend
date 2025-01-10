e1e05e40fc8b6de0f12d9dad08f67e2419f100e5
Dorkilla <dorkilla58@gmail.com>
2025-01-10 14:41:22 +0100

    regisztracio, bejelentkezes, kezdolap felulet elkeszitve

--- konyvcsere_f/src/App.js
+++ konyvcsere_f/src/App.js
@@ -1,9 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Kezdolap from "./pages/Kezdolap";
+import Bejelentkezes from "./pages/Bejelentkezes";
+import Regisztracio from "./pages/Regisztracio";
+import VendegLayout from "./layouts/VendegLayout";
 
     function App() {
       return (
-        
           <Routes>
               <Route path="/" element={<VendegLayout />}>
                   <Route index element={<Kezdolap />} />
