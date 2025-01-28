df047b7177eecb4cb1168844c0b74ba9035e8f49
Dorkilla <dorkilla58@gmail.com>
2025-01-28 16:06:47 +0100

    kezdolap szerkesztese

--- konyvcsere_f/src/pages/Kezdolap.js
+++ konyvcsere_f/src/pages/Kezdolap.js
@@ -5,9 +5,23 @@ export default function Kezdolap(){
 
     //ellenőrizzük van-e bejelentkezett felhasználó
     return(
+        <main className="kezdo">
+            <h1>Kezdőlap</h1>
+        <section class="container">
+        <article>
+        
+        </article>
         <div>
-        <h1>Kezdőlap</h1>
+        <aside>
+        
+        <h2>Üdvözöllek a Könyvcserélő Közösségben!</h2>
+        <p>Képzeld el, hogy egy új könyv mindig csak egy csere távolságra van tőled! Itt az oldalon lehetőséged van arra, hogy a könyveidet másokkal megoszd, és új kedvenceket találj a cserék révén. Legyen szó klasszikus irodalomról, izgalmas krimikről vagy épp inspiráló önfejlesztő olvasmányokról – nálunk minden könyv új kalandot hoz!
+
+Csatlakozz hozzánk, és hozd létre saját könyvcsere-listádat! Cserélj, fedezz fel és ossz meg könyveket, hogy egy szorosabb, olvasásra épülő közösséget építhessünk együtt!</p>
+        </aside>
         <p>Bejelentkezett felhasználó: {user==null?"Nincs bejelentkezett felhasználó":user.full_name}</p>
         </div>
+        </section>
+        </main>
     )
 }
