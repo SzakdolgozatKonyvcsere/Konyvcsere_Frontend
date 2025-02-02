8e45be6b66d337e25ab3e8b7c6a4c7397213e46f
WeimperGergo <weimper.geri@gmail.com>
2025-02-02 21:10:26 +0100

    navbar valtoztatasok + profil link alap benne

--- konyvcsere_f/src/pages/Kezdolap.js
+++ konyvcsere_f/src/pages/Kezdolap.js
@@ -5,28 +5,26 @@ export default function Kezdolap(){
 
     //ellenőrizzük van-e bejelentkezett felhasználó
     return(
-        <main className="kezdo">
-            <h1>Kezdőlap</h1>
-        <section class="container">
-        <article>
-        
-        </article>
-        <div>
-        <aside>
-        
-        <h2>Üdvözöllek a Könyvcserélő Közösségben!</h2>
-        <p>Képzeld el, hogy egy új könyv mindig csak egy csere távolságra van tőled! Itt az oldalon lehetőséged van arra, hogy a könyveidet másokkal megoszd, és új kedvenceket találj a cserék révén. Legyen szó klasszikus irodalomról, izgalmas krimikről vagy épp inspiráló önfejlesztő olvasmányokról – nálunk minden könyv új kalandot hoz!
+        <main className="main-container">
+            {/*<h1>Kezdőlap</h1>*/}
+            <section class="container">
+                <article>
+                
+                </article>
+                <aside>
+                    <h2>Üdvözöllek a Könyvcserélő Közösségben!</h2>
+                    <p>Képzeld el, hogy egy új könyv mindig csak egy csere távolságra van tőled! Itt az oldalon lehetőséged van arra, hogy a könyveidet másokkal megoszd, és új kedvenceket találj a cserék révén. Legyen szó klasszikus irodalomról, izgalmas krimikről vagy épp inspiráló önfejlesztő olvasmányokról – nálunk minden könyv új kalandot hoz! Csatlakozz hozzánk, és hozd létre saját könyvcsere-listádat! Cserélj, fedezz fel és ossz meg könyveket, hogy egy szorosabb, olvasásra épülő közösséget építhessünk együtt!</p>
+                </aside>
+                {/*<p id="authorized-username">{user!==null?user.full_name:"Vendég"}</p>*/}
 
-Csatlakozz hozzánk, és hozd létre saját könyvcsere-listádat! Cserélj, fedezz fel és ossz meg könyveket, hogy egy szorosabb, olvasásra épülő közösséget építhessünk együtt!</p>
-        </aside>
-        <p>Bejelentkezett felhasználó: {user==null?"Nincs bejelentkezett felhasználó":user.full_name}</p>
-        </div>
-        </section>
-        <main className="body__main">
-            <div>
-                <h1>Kezdőlap</h1>
-                <p>Bejelentkezett felhasználó: {user==null?"Nincs bejelentkezett felhasználó":user.full_name}</p>
-            </div>
+            </section>
+            {/*
+            <div className="body__main">
+                <div>
+                    <h1>Kezdőlap</h1>
+                    <p>Bejelentkezett felhasználó: {user==null?"Nincs bejelentkezett felhasználó":user.full_name}</p>
+                </div>
+            </div>*/}
         </main>
-    )
+    );
 }
--- konyvcsere_f/src/pages/Navigacio.js
+++ konyvcsere_f/src/pages/Navigacio.js
@@ -8,7 +8,7 @@ export default function Navigacio(){
 
     return(
         <nav className="navbar navbar-expand-sm bg-light">
-        <div className="container-fluid">
+            <div className="container-fluid">
                 <ul className="navbar-nav">
                     <li className="navbar-item">
                         <Link className="nav-link" to="/">Kezdőlap</Link>
@@ -30,6 +30,10 @@ export default function Navigacio(){
                         </>
                     )}
                 </ul>
+                <div className="navbar-profile">
+                    <p className="user--name">{user!==null?user.full_name:"Vendég"}</p>
+                    <a href="#"><img className="user--profile-picture" alt="felhasználó profilképe" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBTym3IWgvwc58Oq_BCusGZKtqqllkljgw3g&s"></img></a>
+                </div>
             </div>
         </nav>
     );
