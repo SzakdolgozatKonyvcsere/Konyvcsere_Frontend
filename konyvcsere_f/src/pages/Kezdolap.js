ff301972fdd99991d422aa3133a91d9e7a08c3e4
WeimperGergo <weimper.geri@gmail.com>
2025-02-04 09:56:20 +0100

    merge felkesz

--- konyvcsere_f/src/pages/Kezdolap.js
+++ konyvcsere_f/src/pages/Kezdolap.js
@@ -5,26 +5,25 @@ export default function Kezdolap(){
 
     //ellenőrizzük van-e bejelentkezett felhasználó
     return(
-        <main className="kezdo">
-            <h1>Adok-Kapok</h1>
-        <section class="container">
-        <article>
-        </article>
-        <div>
-        <aside>
-        
-        <h2>Üdvözöllek az Adok-Kapok Közösségben!</h2>
-    <ul>
-        <li>Képzeld el, hogy egy új könyv mindig csak egy csere távolságra van tőled!</li>
-        <li>Az oldalon lehetőséged van arra, hogy megoszd könyveidet másokkal, és új kedvenceket találj a cserék révén.</li>
-        <li>Csatlakozz hozzánk, és hozd létre saját könyvcsere-listádat!</li>
-        <li>Cserélj, fedezz fel és ossz meg könyveket, hogy egy szorosabb, olvasásra épülő közösséget építhessünk együtt!</li>
-    </ul>
-        </aside>
-        <p>Bejelentkezett felhasználó: {user==null?"Nincs bejelentkezett felhasználó":user.full_name}</p>
-        </div>
-        </section>
         <main className="main-container">
+        <h1>Adok-Kapok</h1>
+        <section class="container">
+            <article>
+            </article>
+            <div>
+                <aside>             
+                    <h2>Üdvözöllek az Adok-Kapok Közösségben!</h2>
+                    <ul>
+                        <li>Képzeld el, hogy egy új könyv mindig csak egy csere távolságra van tőled!</li>
+                        <li>Az oldalon lehetőséged van arra, hogy megoszd könyveidet másokkal, és új kedvenceket találj a cserék révén.</li>
+                        <li>Csatlakozz hozzánk, és hozd létre saját könyvcsere-listádat!</li>
+                        <li>Cserélj, fedezz fel és ossz meg könyveket, hogy egy szorosabb, olvasásra épülő közösséget építhessünk együtt!</li>
+                    </ul>
+                </aside>
+                <p>Bejelentkezett felhasználó: {user==null?"Nincs bejelentkezett felhasználó":user.full_name}</p>
+            </div>
+        </section>
+        
             {/*<h1>Kezdőlap</h1>*/}
             <section class="container">
                 <article>
