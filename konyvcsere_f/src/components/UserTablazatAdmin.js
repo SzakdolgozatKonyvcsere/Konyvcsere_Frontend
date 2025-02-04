d5c386b4454967fe86006e99680c8f942be0de1a
csorbaeszter26 <csorba.eszter.26@gmail.com>
2025-02-04 14:30:34 +0100

    proba, valszeg hibas userproba

---
+++ konyvcsere_f/src/components/UserTablazatAdmin.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { AllUserContext } from '../contexts/AllUsersContext'
+import {useContext} from 'react'
+import UserSorAdmin from './UserSorAdmin';
+
+export default function TablazatReceptek() {
+    const {userLista}=useContext(AllUserContext);
+    
+  return (
+    <div className="row-cols-1 row-cols-sm-2 row-cols-md-3 g-4">
+      <table className="table">
+        <thead>
+            <tr>
+                <th scope="col">
+                    Név: 
+                </th>
+                <th scope="col">
+                    Email: 
+                </th>
+                <th scope="col">
+                    Teljes név: 
+                </th>
+                <th scope="col">
+                    Város: 
+                </th>
+                <th scope="col">
+                    Telefonszám: 
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            {receptLista.map((user)=>{
+                return <UserSorAdmin user={user} key={user.id} />
+            })}
+        </tbody>
+      </table>
+    </div>
+  )
+}
---
+++ konyvcsere_f/src/contexts/AllUsersContext.js
@@ -0,0 +1,54 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { myAxios } from "../api/axios";
+import { useNavigate } from "react-router-dom";
+
+const AllUsersContext = createContext("");
+
+export const AuthProvider = ({ children }) => {
+
+    //const navigate = useNavigate();
+    const [userLista, setUserLista] = useState(null);
+    
+
+    //
+    const getAdat=async(vegpont, callbackfv)=>{
+        try{
+            console.log("getadat");
+            const response = await myAxios.get(vegpont);
+            callbackfv(response.data)
+        } catch (err) {
+            console.log("Hiba:", err);
+        }finally{
+            
+        }
+
+        }
+
+        const postAdat = async(vegpont,adat)=>{
+            try{
+                const response = await myAxios.post(vegpont,adat);
+                console.log("adat:", response.data)
+            }catch(err){
+                console.log("Hiba",err);
+            }finally{
+
+            }
+        }
+
+        useEffect(()=>{
+            getAdat("osszes-felhasznalo", setUserLista)
+            
+        },[])
+
+
+
+        return (
+            <AllUsersContext.Provider value={{userLista, getAdat, postAdat}}>
+                {children}
+            </AllUsersContext.Provider>
+        )
+
+}
+
+
+
