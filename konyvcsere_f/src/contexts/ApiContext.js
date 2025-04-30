2c56b6f0473a773b9b0e4626ef85a21e898e5086
Dorkilla <dorkilla58@gmail.com>
2025-04-30 21:35:32 +0200

    proba

--- konyvcsere_f/src/contexts/ApiContext.js
+++ konyvcsere_f/src/contexts/ApiContext.js
@@ -376,44 +376,44 @@ export const ApiProvider = ({ children }) => {
     const { data } = await myAxios.get(`/api/admin/exchange-closed?interval=${interval}`);
     return data;
   };
-const getUploadsByCategoryStat = async () => {
-  const { data } = await myAxios.get('/api/admin/book-by-categ');
-  return data;
-};
+  const getUploadsByCategoryStat = async () => {
+    const { data } = await myAxios.get('/api/admin/book-by-categ');
+    return data;
+  };
 
-const getUploadsTrendStat = async (interval = 'daily') => {
-  const { data } = await myAxios.get(`/api/admin/upload-trend?interval=${interval}`);
-  return data;
-};
+  const getUploadsTrendStat = async (interval = 'daily') => {
+    const { data } = await myAxios.get(`/api/admin/upload-trend?interval=${interval}`);
+    return data;
+  };
 
-const getClosedExchangesStat = async (interval = 'daily') => {
-  const { data } = await myAxios.get(`/api/admin/exchange-closed?interval=${interval}`);
-  return data;
-};
+  const getClosedExchangesStat = async (interval = 'daily') => {
+    const { data } = await myAxios.get(`/api/admin/exchange-closed?interval=${interval}`);
+    return data;
+  };
 
-const getExchangeSuccessRatioStat = async () => {
-  const { data } = await myAxios.get('/api/admin/exchange-succes-ratio');
-  return data;
-};
+  const getExchangeSuccessRatioStat = async () => {
+    const { data } = await myAxios.get('/api/admin/exchange-succes-ratio');
+    return data;
+  };
 
-const getAvgExchangeTimeStat = async () => {
-  const { data } = await myAxios.get('/api/admin/exchange-avg-time');
-  return data;
-};
+  const getAvgExchangeTimeStat = async () => {
+    const { data } = await myAxios.get('/api/admin/exchange-avg-time');
+    return data;
+  };
 
-const getTopBooksStat = async () => {
-  const { data } = await myAxios.get('/api/admin/book-top');
-  return data;
-};
+  const getTopBooksStat = async () => {
+    const { data } = await myAxios.get('/api/admin/book-top');
+    return data;
+  };
 
-const getTopAuthorsGenresStat = async () => {
-  const { data } = await myAxios.get('/api/admin/author-genre-top');
-  return data;
-};
-const getMostExchangedCity = async () => {
-  const { data } = await myAxios.get('/api/admin/most-exchanged-city');
-  return data;
-};
+  const getTopAuthorsGenresStat = async () => {
+    const { data } = await myAxios.get('/api/admin/author-genre-top');
+    return data;
+  };
+  const getMostExchangedCity = async () => {
+    const { data } = await myAxios.get('/api/admin/most-exchanged-city');
+    return data;
+  };
 
   const getExchangeSuccessRatioStat = async () => {
     const { data } = await myAxios.get('/api/admin/exchange-succes-ratio');
